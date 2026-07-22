f8b2a1b32d4a70242c7161e8ce034d50a4571cb7
kunxian xia <xiakunxian130@gmail.com>
2026-07-22 20:48:06 +0800

    Allow overriding CIFAR-10 dataset URL

--- cs231n-2025/assignment1/cs231n/datasets/get_datasets.sh
+++ cs231n-2025/assignment1/cs231n/datasets/get_datasets.sh
@@ -1,6 +1,8 @@
 # cd cs231n/datasets
+CIFAR10_URL=${CIFAR10_URL:-http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz}
+
 if [ ! -d "cifar-10-batches-py" ]; then
-  wget http://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz -O cifar-10-python.tar.gz
+  wget "$CIFAR10_URL" -O cifar-10-python.tar.gz
   tar -xzvf cifar-10-python.tar.gz
   rm cifar-10-python.tar.gz
   wget http://cs231n.stanford.edu/imagenet_val_25.npz
