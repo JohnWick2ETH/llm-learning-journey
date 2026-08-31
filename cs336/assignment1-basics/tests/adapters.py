263338d7818625dca14b5dac5fda9a4327c33929
kunxian xia <xiakunxian130@gmail.com>
2026-08-31 17:23:38 +0800

    adamw adapter

--- cs336/assignment1-basics/tests/adapters.py
+++ cs336/assignment1-basics/tests/adapters.py
@@ -23,6 +23,7 @@ from cs336_basics.model import (
     TransformerLM,
 )
 from cs336_basics.utils import softmax, silu, cross_entropy_loss
+from cs336_basics.optimizer import AdamW
 
 
 def run_linear(
@@ -588,7 +589,7 @@ def get_adamw_cls() -> Any:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return AdamW
 
 
 def run_get_lr_cosine_schedule(
