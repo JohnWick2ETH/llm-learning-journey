961ce352ef452966917d509a83a57e9e8856a764
kunxian xia <xiakunxian130@gmail.com>
2026-09-03 00:15:47 +0800

    checkpoint load and save

--- cs336/assignment1-basics/cs336_basics/training.py
+++ cs336/assignment1-basics/cs336_basics/training.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+import os
+import typing
 from random import randint
 
 
@@ -19,3 +21,31 @@ def get_batch(
     )
     nexts.to(device)
     return (inputs, nexts)
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+):
+    torch.save(
+        {
+            "model": model.state_dict(),
+            "optimizer": optimizer.state_dict(),
+            "iteration": iteration,
+        },
+        out,
+    )
+
+
+def load_checkpoint(
+    src: str | os.PathLike | typing.BinaryIO | typing.IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint["model"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+
+    return checkpoint["iteration"]
